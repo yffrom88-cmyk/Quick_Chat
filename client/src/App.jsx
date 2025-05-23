2dad72bad5ae3f30162327f745bd532aaafc4ea2
SRCarlo <asphaltshubhuu@gmail.com>
2025-05-23 14:23:15 +0530

    server.js updated for versal

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,23 +1,32 @@
-import React, { useContext } from 'react'
-import { Navigate, Route, Routes } from 'react-router-dom'
-import { HomePage } from './Pages/HomePage'
-import LoginPage from './Pages/LoginPage'
-import ProfilePage from './Pages/ProfilePage'
-import { Toaster } from 'react-hot-toast'
-import { AuthContext } from '../context/AuthContext'
+import React, { useContext } from "react";
+import { Navigate, Route, Routes } from "react-router-dom";
+import { HomePage } from "./Pages/HomePage";
+import LoginPage from "./Pages/LoginPage";
+import ProfilePage from "./Pages/ProfilePage";
+import { Toaster } from "react-hot-toast";
+import { AuthContext } from "../context/AuthContext";
 
 const App = () => {
-  const { authUser } = useContext(AuthContext)
+  const { authUser } = useContext(AuthContext);
   return (
-    <div className="bg-[url('./src/assets/bgImage.jpg')] bg-contain">
+    <div className="bg-[url('/bgImage.jpg')] bg-contain">
       <Toaster />
       <Routes>
-        <Route path='/' element={authUser ? <HomePage /> : <Navigate to="/login" />} />
-        <Route path='/login' element={!authUser ? <LoginPage /> : <Navigate to="/" />} />
-        <Route path='/profile' element={authUser ? <ProfilePage /> : <Navigate to="/login" />} />
+        <Route
+          path="/"
+          element={authUser ? <HomePage /> : <Navigate to="/login" />}
+        />
+        <Route
+          path="/login"
+          element={!authUser ? <LoginPage /> : <Navigate to="/" />}
+        />
+        <Route
+          path="/profile"
+          element={authUser ? <ProfilePage /> : <Navigate to="/login" />}
+        />
       </Routes>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
